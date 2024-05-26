5f37a65f5d343d02330f90af11c9aa517dec8bec
Survi Suman <survisuman@Survis-MacBook-Pro.local>
2024-05-27 01:18:20 +0530

    initial commit

---
+++ dist/cv/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-FR5HLCZF.mjs";import"./chunk-IEPFXLTA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- plugins.sh
+++ plugins.sh
@@ -1,2 +1,10 @@
 # steps to deploy on git
-ng add angular-cli-ghpages
+
+# ng add angular-cli-ghpages
+# git remote add origin https://github.com/sumansurvi/cv.git
+# push code to repository
+# build app using below command
+# ng build --base-href "https://sumansurvi.github.io/cv/"
+
+#deploy
+# npx angular-cli-ghpages --dir=dist/cv
