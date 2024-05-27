6da1ed2c09ed57224bbb8959d9f414351a1b035b
Survi Suman <survisuman@Survis-MacBook-Pro.local>
2024-05-27 16:53:47 +0530

    modified content

--- plugins.sh
+++ plugins.sh
@@ -4,5 +4,6 @@
 # git remote add origin https://github.com/sumansurvi/cv.git
 # push code to repository
 # build app using below command
-
-# ng build --output-path docs --base-href /repository-name/
+# ng build --base-href "https://sumansurvi.github.io/cv/"
+#deploy
+# npx angular-cli-ghpages --dir=dist/cv/browser
